639069fa6804ce508bf1c8a1ddfb85d3e78991c3
Sameer-Shahzad <sameershahzad835@gmail.com>
2026-01-22 14:10:21 +0500

    twentyThird commit

--- carts/views.py
+++ carts/views.py
@@ -16,6 +16,10 @@ def _cart_id (request):
     return cart
 
 def add_cart (request, product_id):
+    color = request.GET['color']
+    # size = request.GET['size']
+    return HttpResponse(color)
+    
     product = Product.objects.get(id=product_id) 
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request))
--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import Product, Variation
 
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('product_name',)}
     list_display = ('product_name', 'price','stock', 'modified_date', 'is_available', 'slug')
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active', 'created_date')
+    list_editable = ('is_active',)
+    
 admin.site.register(Product, ProductAdmin) 
+admin.site.register(Variation, VariationAdmin)
