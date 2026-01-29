0a3e47ff31052e2ca61d8e491ea3a53a5608769a
Sameer-Shahzad <sameershahzad835@gmail.com>
2026-01-29 20:33:19 +0500

    twentySeventh commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,54 @@
-from django.shortcuts import render
+import re
+from django.forms import ValidationError
+from django.shortcuts import redirect, render
 from .models import Account
 from .forms import RegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 
-def register (request):
+def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-           first_name = form.cleaned_data.get('first_name')
-           last_name = form.cleaned_data.get('last_name')
-           email = form.cleaned_data.get('email')
-        #    phone_number = form.cleaned_data.get('phone_number')
-           password1 = form.cleaned_data.get('password1')
-           password2 = form.cleaned_data.get('password2')
-           
-           if password1 == password2:
-               pass
-           else:
-               form.add_error('password2', 'Passwords do not match')
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            email = form.cleaned_data.get('email')
+            password1 = form.cleaned_data.get('password1')
+            password2 = form.cleaned_data.get('password2')
 
-        user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=email.split('@')[0], password=password1)
-        user.save()
+            if not re.match(r'^[a-zA-Z\s]+$', first_name):
+                form.add_error('first_name', "Name should only contain letters!")
+            
+            if not re.match(r'^[a-zA-Z\s]+$', last_name):
+                form.add_error('last_name', "Last name should only contain letters!")
+
+            if Account.objects.filter(email=email).exists():
+                form.add_error('email', 'This email is already registered!')
+
+            if password1 != password2:
+                form.add_error('password2', 'Passwords do not match!')
+
+            if not form.errors:
+                base_username = email.split('@')[0]
+                username = base_username
+                counter = 1
+                while Account.objects.filter(username=username).exists():
+                    username = f"{base_username}{counter}"
+                    counter += 1
+                
+                user = Account.objects.create_user(
+                    first_name=first_name, 
+                    last_name=last_name, 
+                    email=email, 
+                    username=username, 
+                    password=password1
+                )
+                user.save()
+                messages.success(request, 'Account created successfully!')
+                return redirect('register')
+            
+            
     else:
         form = RegistrationForm()
         
