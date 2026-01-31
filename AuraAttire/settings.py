fc7207bbcddeb76da56fbfd4be703ab1340e985c
Sameer-Shahzad <sameershahzad835@gmail.com>
2026-01-31 23:29:02 +0500

    36th commit

--- AuraAttire/settings.py
+++ AuraAttire/settings.py
@@ -148,10 +148,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = os.getenv("EMAIL_USER")
-# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASS")
-EMAIL_HOST_USER = 'sameershahzad835@gmail.com'
-EMAIL_HOST_PASSWORD = 'alep alik uhgj uafy'
+EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASS")
+
 # EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
-secure = False
