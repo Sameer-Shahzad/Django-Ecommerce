0cdc7c1cc62ad788fcec18beca3711be787b1f4b
Sameer-Shahzad <sameershahzad835@gmail.com>
2026-03-17 00:19:09 +0500

    Updated footer name

--- AuraAttire/urls.py
+++ AuraAttire/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.static import serve 
+from django.urls import path, include
+from django.conf.urls.static import static 
 from . import views
 
 urlpatterns = [
@@ -12,5 +12,4 @@ urlpatterns = [
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
